c3b9acc6521daaeddc5df957c53410719c47117c
Ishan <ishananand930@gmail.com>
2026-01-27 21:45:50 +0530

    Spring

--- src/main/java/com/spr/App.java
+++ src/main/java/com/spr/App.java
@@ -1,7 +1,5 @@
 package com.spr;
-
 import javax.swing.Spring;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -10,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        // “Bean” means a normal Java object.
+    // “Bean” means a normal Java object.
     // Spring Bean” means a Java object that is created and managed by Spring.
 
 //     ApplicationContext is a Spring container interface that defines methods for creating and managing beans.
@@ -23,7 +21,13 @@ public class App
 // Manages its lifecycle -> when it is created or when it is destroyed 
 // managing = creating + storing + injecting + lifecycle control
 // Because all this work is done by Spring, that object is called a:  // Spring Bean
+
         ApplicationContext ac=new ClassPathXmlApplicationContext("com/spr/hello.xml");
+
+        // getBean() returns an object of type Object.
+        // But we want to store that returned object into a Student reference variable.
+        // Since a Student variable can only hold Student type objects,
+        // we must typecast the Object into Student before storing it.
         Student so1=(Student) ac.getBean("s1");
         Student so2=(Student) ac.getBean("s2");
         System.out.println(so1);
