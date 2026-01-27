fc2f9913a353e0eab6d161bef16db584802fad2e
Ishan <ishananand930@gmail.com>
2026-01-27 19:46:25 +0530

    Spring core

---
+++ src/main/java/com/spr/App.java
@@ -0,0 +1,32 @@
+package com.spr;
+
+import javax.swing.Spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // “Bean” means a normal Java object.
+    // Spring Bean” means a Java object that is created and managed by Spring.
+
+//     ApplicationContext is a Spring container interface that defines methods for creating and managing beans.
+// ClassPathXmlApplicationContext is a class that implements this interface, loads and reads the XML configuration file from the classpath, and provides the implementation of those methods
+// What you mean is:
+// Spring:
+// Creates the object
+// Stores it in the container
+// Injects it where needed
+// Manages its lifecycle -> when it is created or when it is destroyed 
+// managing = creating + storing + injecting + lifecycle control
+// Because all this work is done by Spring, that object is called a:  // Spring Bean
+        ApplicationContext ac=new ClassPathXmlApplicationContext("com/spr/hello.xml");
+        Student so1=(Student) ac.getBean("s1");
+        Student so2=(Student) ac.getBean("s2");
+        System.out.println(so1);
+        System.out.println(so2);
+    }
+}
---
+++ src/main/java/com/spr/Student.java
@@ -0,0 +1,48 @@
+package com.spr;
+public class Student {
+	private int roll;
+	private String name;
+	private String city;
+	
+	
+//	default constructor
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+//	print karne ke liye toString() method bna diya h
+// object ko jayese hmm print karte h app.java me toh by default yehi method call hota h or print hota h
+@Override
+	public String toString() {
+		return "Student [roll=" + roll + ", name=" + name + ", city=" + city + "]";
+	}
+
+	//	parametrized constructor
+	public Student(int roll,String name,String city) {
+		super();
+		this.roll=roll;
+		this.name=name;
+		this.city=city;
+	}
+	public void setRoll(int roll) {
+		this.roll=roll;
+	}
+	public int getRoll() {
+		return roll;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setCity(String city) {
+		this.city=city;
+	}
+	public String getCity() {
+		return city;
+	}
+	
+}
